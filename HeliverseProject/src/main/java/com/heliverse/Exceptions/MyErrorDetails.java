ff8327febb4d8df5f6f1bec7c849373dc29bbc52
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-14 18:18:28 +0530

    Error details class

---
+++ HeliverseProject/src/main/java/com/heliverse/Exceptions/MyErrorDetails.java
@@ -0,0 +1,13 @@
+package com.heliverse.Exceptions;
+
+public class MyErrorDetails extends RuntimeException{
+	
+	
+	public MyErrorDetails(String message) {
+		super(message);
+	}
+	public MyErrorDetails() {
+		
+	}
+
+}
