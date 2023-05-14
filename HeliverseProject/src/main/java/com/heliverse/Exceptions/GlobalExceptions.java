de5a42c5b6635c273a8c846de584c410ce364d4e
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-14 21:12:44 +0530

    Global exception class

--- HeliverseProject/src/main/java/com/heliverse/Exceptions/GlobalExceptions.java
+++ HeliverseProject/src/main/java/com/heliverse/Exceptions/GlobalExceptions.java
@@ -1,6 +1,12 @@
 package com.heliverse.Exceptions;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptions {
@@ -9,9 +15,21 @@ public class GlobalExceptions {
 	public ResponseEntity<MyErrorDetails> myAnyExpHandler(Exception ie,WebRequest req){
 
 		MyErrorDetails err = new MyErrorDetails();
-		err.setTimestamp(LocalDateTime.now());
+		err.setLcdt(LocalDateTime.now());
 		err.setMessage(ie.getMessage());
-		err.setDetails(req.getDescription(false));
+		err.setDescription(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MyErrorDetails> QuizeExceptionHandler(QuizeExceptions qz,WebRequest req){
+
+		MyErrorDetails err = new MyErrorDetails();
+		err.setLcdt(LocalDateTime.now());
+		err.setMessage(qz.getMessage());
+		err.setDescription(req.getDescription(false));
 		
 		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
 		
