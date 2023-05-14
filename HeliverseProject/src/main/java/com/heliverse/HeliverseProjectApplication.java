9a6b927fed018906872479074c9ace9bef58faed
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-14 17:57:05 +0530

    starting of project

---
+++ HeliverseProject/src/main/java/com/heliverse/HeliverseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.heliverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliverseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliverseProjectApplication.class, args);
+	}
+
+}
