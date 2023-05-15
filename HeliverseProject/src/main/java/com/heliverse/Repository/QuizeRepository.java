a79fe90fcf3cb64d64a6bf4007eabd9cb547de0d
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-15 10:15:57 +0530

    time difference method

--- HeliverseProject/src/main/java/com/heliverse/Repository/QuizeRepository.java
+++ HeliverseProject/src/main/java/com/heliverse/Repository/QuizeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.heliverse.Models.Quize;
@@ -13,5 +14,8 @@ public interface QuizeRepository extends JpaRepository<Quize, Integer>{
 	
 	@Query(value = "SELECT * from quize q where CURRENT_DATE() BETWEEN start_Date AND end_Date AND CURRENT_TIME() BETWEEN start_Time AND end_Time;",nativeQuery = true)
 	List<Quize> getActiveQuize();
+	
+	@Query(value = "Select TIMESTAMPDIFF(MINUTE,end_Time,CURRENT_TIME()) from quize q where q.quize_Id = :id",nativeQuery = true)
+	Integer getTimeDiff( Integer id);
 
 }
