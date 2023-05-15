f0ad5e51c01c95fad3e5297818b55c671ea93961
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-15 10:17:18 +0530

    comment

--- HeliverseProject/src/main/java/com/heliverse/Repository/QuizeRepository.java
+++ HeliverseProject/src/main/java/com/heliverse/Repository/QuizeRepository.java
@@ -12,9 +12,12 @@ import com.heliverse.Models.Quize;
 @Repository
 public interface QuizeRepository extends JpaRepository<Quize, Integer>{
 	
+	//active quize
 	@Query(value = "SELECT * from quize q where CURRENT_DATE() BETWEEN start_Date AND end_Date AND CURRENT_TIME() BETWEEN start_Time AND end_Time;",nativeQuery = true)
 	List<Quize> getActiveQuize();
 	
+	
+	//Minute difference betwenn current time and end time
 	@Query(value = "Select TIMESTAMPDIFF(MINUTE,end_Time,CURRENT_TIME()) from quize q where q.quize_Id = :id",nativeQuery = true)
 	Integer getTimeDiff( Integer id);
 
