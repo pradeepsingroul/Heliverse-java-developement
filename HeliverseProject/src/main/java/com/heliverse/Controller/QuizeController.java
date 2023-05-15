cb11c45ae69243f2f7330f7c6272a646f7f2ad61
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-15 10:14:40 +0530

    Comments

--- HeliverseProject/src/main/java/com/heliverse/Controller/QuizeController.java
+++ HeliverseProject/src/main/java/com/heliverse/Controller/QuizeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.heliverse.Models.Quize;
+import com.heliverse.Models.Result;
 import com.heliverse.Services.QuizeService;
 
 import jakarta.annotation.PostConstruct;
@@ -24,29 +25,39 @@ public class QuizeController {
 	@Autowired
 	private QuizeService qService;
 	
-	
-	@GetMapping("/active")
+	//getting the active quizes
+	@GetMapping("/active") 
 	public ResponseEntity<List<Quize>> getActiveQuizeController(){
 		List<Quize> activeQuizelList = qService.getActiveQuizeService();
 		return new ResponseEntity<>(activeQuizelList,HttpStatus.OK);
 	}
 	
+	//creating new quize
 	@PostMapping("/create-quize")
 	public ResponseEntity<Quize> createQuize(@RequestBody Quize quize){
 		Quize qz = qService.createQuize(quize);
 		return new ResponseEntity<Quize>(qz,HttpStatus.OK);
 	}
 	
+	//getting all quizes 
 	@GetMapping("/getAll")
 	public ResponseEntity<List<Quize>> getAllQuizeController(){
 		List<Quize> allQuizeList = qService.getAllQuize();
 		return new ResponseEntity<>(allQuizeList,HttpStatus.OK);
 	}
 	
-	@GetMapping("/{id}/result")
-	public ResponseEntity<Quize> getResultByID(@PathVariable Integer id){
+	//getting the quize by id
+	@GetMapping("/{id}")
+	public ResponseEntity<Quize> getByID(@PathVariable Integer id){
 		Quize quize = qService.getByID(id);
 		return new ResponseEntity<>(quize,HttpStatus.OK);
+	}
+	
+	//getting the quize result by id
+	@GetMapping("/{id}/result")
+	public ResponseEntity<Result> getResultById(@PathVariable Integer id){
+		Result result = qService.getResultByID(id);
+		return new ResponseEntity<>(result,HttpStatus.OK);
 	}
 	
 
