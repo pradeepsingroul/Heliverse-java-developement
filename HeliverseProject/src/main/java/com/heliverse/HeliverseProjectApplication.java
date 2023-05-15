49d96ab3d14d6e2ffb9a4a045d433157ff51a35e
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-15 15:48:00 +0530

    Enable cron job

--- HeliverseProject/src/main/java/com/heliverse/HeliverseProjectApplication.java
+++ HeliverseProject/src/main/java/com/heliverse/HeliverseProjectApplication.java
@@ -1,13 +1,23 @@
 package com.heliverse;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.heliverse.Models.Quize;
+import com.heliverse.Services.QuizeService;
+
+@EnableScheduling
 @SpringBootApplication
 public class HeliverseProjectApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeliverseProjectApplication.class, args);
+		
 	}
 
 }
